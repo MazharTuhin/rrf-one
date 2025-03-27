b19d3dde6c4fa66aa1b3807f6cd2c3826c9b1f7f
Mazhar Tuhin <mazhartuhin78@gmail.com>
2025-03-27 23:39:26 +0600

    Initial Commit after complete project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#FFFFFF',
+      'gray': '#F7F7F7',
+      'medium-gray': '#E1E1E1',
+      'dark-gray': '#8D8D8D',
+      'dark': '#1F1F1F',
+      'pink': '#FFD4D0',
+      'red': '#FC3314',
+    },
+  },
+  plugins: [],
+}
+
